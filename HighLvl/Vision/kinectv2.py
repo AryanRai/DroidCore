d593acd37b94f99456cbcb93525807542b7a16e1
aryanyan <31175254+AryanRai@users.noreply.github.com>
2025-04-01 03:32:16 +1100

    Sync

--- HighLvl/Vision/kinectv2.py
+++ HighLvl/Vision/kinectv2.py
@@ -3,7 +3,7 @@ import numpy as np
 import subprocess
 
 # Start Protonect (libfreenect2 viewer) in the background
-proc = subprocess.Popen([".libfreenect2/bin/Protonect", "cpu"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc = subprocess.Popen(["./libfreenect2/bin/Protonect", "cpu"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Wait for Protonect to initialize, then capture frames
 while True:
